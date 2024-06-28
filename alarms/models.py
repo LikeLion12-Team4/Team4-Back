5a26bb1aaa66ebde88c97f1ad0eeefa8d8db78d9
leeeemj <leemj2944@gmail.com>
2024-06-28 12:12:10 +0900

    fit : alarms/models.py
    VIdeo, User 아직 모델 없어서 마이그레이션 안 해놓음

--- alarms/models.py
+++ alarms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from users.models import User
+from videos.models import Video
 # Create your models here.
+class Alarm(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='alarms',null=True)
+    video = models.ForeignKey(Video,on_delete=models.CASCADE,related_name='alarms',null=True)
+    content=models.CharField(max_length=50,null=True)
+    interval = models.IntegerField(default=60,null=True) #알람간격 디폴트값 설정 필요
+    image=models.ImageField(upload_to='alarms/', height_field=200, width_field=200, max_length=None,null=True)
+    is_alarm=models.BooleanField(null=False,default=True) #디폴트값 설정 
+    is_volumn=models.BooleanField(null=False,default=True)
