21f47ae31581145b13853d5b0d9b47b53729e540
leeeemj <leemj2944@gmail.com>
2024-06-29 19:24:43 +0900

    feat : alarms/serializers.py

--- alarms/models.py
+++ alarms/models.py
@@ -6,7 +6,7 @@ class Alarm(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='alarms',null=True)
     video = models.ForeignKey(Video,on_delete=models.CASCADE,related_name='alarms',null=True)
     content=models.CharField(max_length=50,null=True)
-    interval = models.IntegerField(default=60,null=True) #알람간격 디폴트값 설정 필요
+    interval = models.IntegerField(default=60,null=True) #알람간격 60분 디폴트 설정 -> 디폴트값 설정 정하기 
     image=models.ImageField(upload_to='alarms/', height_field=200, width_field=200, max_length=None,null=True)
-    is_alarm=models.BooleanField(null=False,default=True) #디폴트값 설정 
+    is_alarm=models.BooleanField(null=False,default=True) #알람o 소리o 디폴트 설정 
     is_volumn=models.BooleanField(null=False,default=True)
