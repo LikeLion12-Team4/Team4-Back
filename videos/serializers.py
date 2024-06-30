d9c8acc948e264ab72e56acdc4f48cdcbd782790
JUNE <jh010303@naver.com>
2024-06-30 21:48:22 +0900

    feat : videos/serializers.py
    BodyPartSerializer 모델 변경으로 인한 수정

--- videos/serializers.py
+++ videos/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from videos.models import BodyPart,Video
 from users.models import VideoLike
 
-class BodyPartSerializer(serializers.ModelSerializer): # 테스트 완료
+class BodyPartSerializer(serializers.ModelSerializer): 
     class Meta:
         model = BodyPart
-        fields = ['id','neck','wrist','eye','waist','field']
+        fields = ['id','partname']
 
-class VideoSerializer(serializers.ModelSerializer): # 테스트 완료 / thumbnail 테스트 방법을 모르겠음..
+class VideoSerializer(serializers.ModelSerializer): # thumbnail 테스트 방법을 모르겠음..
     videolikes_num = serializers.SerializerMethodField()
 
     bodypart = BodyPartSerializer()
@@ -16,5 +16,5 @@ class VideoSerializer(serializers.ModelSerializer): # 테스트 완료 / thumbna
         model = Video
         fields = ['id','title','length','youtubelink','thumbnail','bodypart','videolikes_num']
     
-    def get_videolikes_num(self,obj): # 동영상의 좋아요 개수 반환, postman으로 test하기
+    def get_videolikes_num(self,obj): # 동영상의 좋아요 개수 반환
         return VideoLike.objects.filter(video=obj).count()
