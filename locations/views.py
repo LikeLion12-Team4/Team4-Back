feb01cd9918299d9c606e877c5956e080b5b9c2c
leeeemj <leemj2944@gmail.com>
2024-07-02 13:38:39 +0900

    fix : locations/views.py
    경계값->현재 위치값 받아오는 것으로 변경

--- locations/views.py
+++ locations/views.py
@@ -13,17 +13,14 @@ from django.db.models import Q
 #권한 있어야만 지도 볼 수 있는지? 비회원도 지도는 볼 수 있게끔 하면 좋을듯 
 class LocationListView(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
-        lat_start = request.GET.get('lat_start',None)
-        lat_end = request.GET.get('lat_end',None)
-        long_start = request.GET.get('long_start',None)
-        long_end = request.GET.get('long_end',None)
+        lat = request.GET.get('lat',None)
+        long = request.GET.get('long',None)
         #사용자 현재 위치만 주변 데이터들만 필터링하기 위해 Q()와 쿼리스트링 사용 
         q = Q()
-        if lat_start and lat_end and long_start and long_end:
-            q = Q(latitude__gte=lat_start) & Q(latitude__lte=lat_end)
-            q &= Q(longitude__gte=long_start) & Q(longitude__lte=long_end)
+        if lat and long:
+            q = Q(latitude__gte=lat-100) & Q(latitude__lte=lat+100)
+            q &= Q(longitude__gte=long-30) & Q(longitude__lte=long+30)
 
-        
         hospitals = Hospital.objects.filter(q)
         lessons = Lesson.objects.filter(q)
 
