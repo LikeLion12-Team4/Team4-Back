4fbc45e2ec3d1dd20e1c306893a7a20682d36d02
leeeemj <leemj2944@gmail.com>
2024-07-03 13:06:32 +0900

    fix : alarm앱 모델, 시리얼라이저 수정

--- alarms/models.py
+++ alarms/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from users.models import User
 from videos.models import Video
 # Create your models here.
-class Alarm(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='alarms',null=True)
-    video = models.ForeignKey(Video,on_delete=models.CASCADE,related_name='alarms',null=True)
-    content=models.CharField(max_length=50,null=True)
+class Option(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='options',null=True)
     interval = models.IntegerField(default=60,null=True) #알람간격 60분 디폴트 설정 -> 디폴트값 설정 정하기 
-    image=models.ImageField(upload_to='alarms/', height_field=200, width_field=200, max_length=None,null=True)
     is_alarm=models.BooleanField(null=False,default=True) #알람o 소리o 디폴트 설정 
     is_volumn=models.BooleanField(null=False,default=True)
+
+class AlarmContent(models.Model):
+    content=models.CharField(max_length=50,null=True)
+    image=models.ImageField(upload_to='alarms/',max_length=None,null=True)
+
+# class Alarm(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='alarms',null=True)
+#     video = models.ForeignKey(Video,on_delete=models.CASCADE,related_name='alarms',null=True)
+#     content = models.ForeignKey(AlarmContent,on_delete=models.CASCADE,related_name='alarms',null=True)
+#     option = models.ForeignKey(Option,on_delete=models.CASCADE,related_name='alarms',null=True)
+    
+
+
--- alarms/serializers.py
+++ alarms/serializers.py
@@ -1,16 +1,34 @@
 from rest_framework import serializers
-from alarms.models import Alarm
-from users.serializers import UserSerializer
-from videos.serializers import VideoSerializer
+from alarms.models import AlarmContent
+from alarms.models import Option
+# from alarms.models import Alarm
 
-class AlarmSerializer(serializers.ModelSerializer):
-    user=UserSerializer()
-    video=serializers.SlugRelatedField(
-        many=False,
-        read_only=True,
-        slug_field='youtubelink'
-    ) #video링크만 시리얼라이저
-
+class AlarmContentSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Alarm
+        model=AlarmContent
         fields='__all__'
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Option
+        fields='__all__'
+    
+# class AlarmSerializer(serializers.ModelSerializer):
+#     # user=UserSerializer()
+#     # video=VideoSerializer()
+#     option=OptionSerializer()
+#     content=AlarmContentSerializer
+
+#     user=serializers.SlugRelatedField(
+#         many=False,
+#         read_only=True,
+#         slug_field='username'
+#     )
+#     video=serializers.SlugRelatedField(
+#         many=False,
+#         read_only=True,
+#         slug_field='youtubelink'
+#     ) #video링크만 시리얼라이저
+#     class Meta:
+#         model=Alarm
+#         fields='__all__'
