0cd2bc9a0c302c9f7273ac16d0cda33c776648f4
leeeemj <leemj2944@gmail.com>
2024-07-05 05:41:43 +0900

    Comment : 주석 삭제

--- alarms/serializers.py
+++ alarms/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from alarms.models import AlarmContent
 from alarms.models import Option
-# from alarms.models import Alarm
 
 class AlarmContentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,23 +11,3 @@ class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model=Option
         fields='__all__'
-    
-# class AlarmSerializer(serializers.ModelSerializer):
-#     # user=UserSerializer()
-#     # video=VideoSerializer()
-#     option=OptionSerializer()
-#     content=AlarmContentSerializer
-
-#     user=serializers.SlugRelatedField(
-#         many=False,
-#         read_only=True,
-#         slug_field='username'
-#     )
-#     video=serializers.SlugRelatedField(
-#         many=False,
-#         read_only=True,
-#         slug_field='youtubelink'
-#     ) #video링크만 시리얼라이저
-#     class Meta:
-#         model=Alarm
-#         fields='__all__'
