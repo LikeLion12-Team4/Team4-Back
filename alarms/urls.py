871de567567521224e0a63a74034a6d9f86ad801
leeeemj <leemj2944@gmail.com>
2024-07-05 05:43:51 +0900

    Refactor: alarms view 수정
    이미지 업로드 뷰 추가, 다른 뷰들 수정

--- alarms/urls.py
+++ alarms/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from alarms import views
 
 urlpatterns=[
-    path('', views.AlarmSetView.as_view(), name='option'),
-    path('<int:user_id>/', views.OptionView.as_view(), name='option'),
-    path('message/<int:user_id>/', views.AlarmView.as_view(), name='option'),
-    path('content/', views.contentcreate, name='contentcreate'),
+    path('', views.AlarmSetView.as_view(), name='alarm-set'),
+    path('option/', views.OptionView.as_view(), name='option'),
+    path('message/', views.MessageView.as_view(), name='alarm'),
+    
 ] 
--- alarms/views.py
+++ alarms/views.py
@@ -2,84 +2,85 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from django.shortcuts import redirect
+from rest_framework import views
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework import exceptions
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+
 
 from alarms.serializers import AlarmContentSerializer
 from alarms.models import Option
 from alarms.serializers import OptionSerializer
-from users.models import User
 from videos.models import Video
 from videos.serializers import VideoSerializer
 from alarms.models import AlarmContent
+from alarms.permissions import IsOwner
 
 # Create your views here.
-#알람 설정 여부 확인
-#video title과 링크, username 필요 
-#video도 랜덤 
-#content-이미지세트는 랜덤 -> 총 세가지 => 따로 엔티티 만들어서 랜덤 돌리기
-
-#알람 초기 설정 
+#알람 초기 설정 (추가)
+#로그인 유저만
 class AlarmSetView(generics.CreateAPIView):
+    permission_classes=[IsAuthenticated]
     serializer_class=OptionSerializer
     def perform_create(self, serializer):
-        TEST_USER=User.objects.get(id=1)
-        serializer.save(user=TEST_USER)
-        # serializer.save(user=self.request.user)
+        if Option.objects.filter(user=self.request.user).exists(): #option은 일대일관계로 이미 존재하면 생성할 때 오류남
+            raise exceptions.ValidationError({'error': '이미 존재'}) #그래서 400에러 발생하도록
+        serializer.save(user=self.request.user,is_option=True) #생성될때 True값으로 바뀌도록 하여서 설정 여부 확인할 수 있게
 
+        
 
-#알림 설정 확인 및 수정
+#알림 설정 확인 및 수정 (변경)
+#로그인0,해당 유저만
 class OptionView(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Option.objects.all()
-    serializer_class=OptionSerializer
-    lookup_field='id'
-    lookup_url_kwarg='user_id'
-
-    def retrieve(self, request, *args, **kwargs):
+    # permission_classes=[IsOwner]
+    permission_classes=[IsAuthenticated]
+    #url 파라미터 사용 안 하는 방향으로 변경 => 자기 알림만 확인 0 =? 그럼 permission 필요 없어지나?? 근데 애초에 로그인해야 알림 설정할 수 있는 화면 나타나긴 함
+    serializer_class = OptionSerializer
+    def get_object(self):
         try:
-            instance = self.get_object()
-        except:
-            #초기 설정 안 되어있을 때 이렇게 뜨는게 프론트한테 나을듯
-            return Response({'알림 초기 설정':False},status=status.HTTP_404_NOT_FOUND)
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-  
+            return Option.objects.get(user=self.request.user)
+        except Option.DoesNotExist:
+            raise exceptions.NotFound({'error':'알람 설정 존재x'}) #404에러
+    #is_option필드로 알람 설정여부 확인 가능 
 
-#로그인이 되어있고, 알람 설정했으면 알람 확인 가능 
+        
+
+#로그인이 되어있고, 알람 설정했으면 알람 확인 가능 =>로그인 여부 : permission 사용, 알람 설정 여부 : 프론트에서 확인 ?
 #알람 메세지 가져오기(만들기)
-class AlarmView(generics.RetrieveAPIView):
-    #비디오, content 다 그때마다 랜덤으로 가져오니까 알람모델 만들어서 인스턴스 생성할 필요 없을듯 
-    #알람 모델, 시리얼라이저 다 일단 주석처리
-    #근데 이렇게 하니까 상속을 전혀 활용하지 못하는 ........
-
+class MessageView(generics.RetrieveAPIView):
+    permission_classes=[IsAuthenticated]
     def get(self, request, *args, **kwargs): 
-        #해당 유저
-        user_id = kwargs.get('user_id')
-        user = get_object_or_404(User, id=user_id)
-        #로그인 구현되면 request.user.username으로 구현 예정 
-
         #비디오 랜덤 설정
         video=Video.objects.order_by("?").first()
-        video_serializer=VideoSerializer(video) #비디오 title과 youtubelink가 필요 => 랜덤으로 가져오게 되면서 이걸 맞게 가져오게할 시리얼라이저가 없음 ..
+        video_serializer=VideoSerializer(video)
         #content 랜덤설정 
         content=AlarmContent.objects.order_by("?").first()
         content_serializer=AlarmContentSerializer(content)
-        
         #title과 link만 가져오기
         video_data = {
             'title': video_serializer.data.get('title'),
             'youtubelink': video_serializer.data.get('youtubelink')
         }
-
         data={
-            'username':user.username,
+            'username':request.user.username,
             'content':content_serializer.data,
             'video':video_data,
         } 
         return Response(data,status=status.HTTP_200_OK)
 
+#content 생성 뷰(이미지 업로드)
+#관리자만
+class AlarmContentView(views.APIView):
+    permission_classes=[IsAdminUser]
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = AlarmContentSerializer
 
-
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST )
     
 
 
