6f21253760043a86917ab7e1039b3ae7b3b828d9
leeeemj <leemj2944@gmail.com>
2024-07-05 05:40:09 +0900

    refactor : alarms model변경
    is_option 필드 추가함 -> 알람설정 여부 확인 가능하도록

--- alarms/models.py
+++ alarms/models.py
@@ -4,6 +4,7 @@ from videos.models import Video
 # Create your models here.
 class Option(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='options',null=True)
+    is_option=models.BooleanField(null=False,default=False) #초기 설정 여부 확인 필드 추가, 디폴트 = False
     interval = models.IntegerField(default=60,null=True) #알람간격 60분 디폴트 설정 -> 디폴트값 설정 정하기 
     is_alarm=models.BooleanField(null=False,default=True) #알람o 소리o 디폴트 설정 
     is_volumn=models.BooleanField(null=False,default=True)
@@ -12,11 +13,7 @@ class AlarmContent(models.Model):
     content=models.CharField(max_length=50,null=True)
     image=models.ImageField(upload_to='alarms/',max_length=None,null=True)
 
-# class Alarm(models.Model):
-#     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='alarms',null=True)
-#     video = models.ForeignKey(Video,on_delete=models.CASCADE,related_name='alarms',null=True)
-#     content = models.ForeignKey(AlarmContent,on_delete=models.CASCADE,related_name='alarms',null=True)
-#     option = models.ForeignKey(Option,on_delete=models.CASCADE,related_name='alarms',null=True)
-    
+    def __str__(self):
+        return self.content
 
 
