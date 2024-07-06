acd83f783f7a2a7e4f5e21be28a665f6f272c629
JUNE <jh010303@naver.com>
2024-07-07 00:38:40 +0900

    fix : users/views.py

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from users.models import User,BodyPart,VideoLike
-from users.serializers import UserSerializer,BodyPartSerializer,VideoLikeSerializer
+from users.models import User,BodyPart,VideoLike,Email
+from users.serializers import UserSerializer,BodyPartSerializer,VideoLikeSerializer,EmailSerializer
 from videos.models import Video
 
 from rest_framework import viewsets
@@ -39,7 +39,6 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = 'id'
     lookup_url_kwarg = 'user_id' 
     #permission_classes = [IsOwner] 
-    random_num = f"{randint(0,999999):06}"
     
     # 로그인
     @action(methods=['POST'],detail = False, url_path='login',url_name='user-login')
@@ -50,7 +49,7 @@ class UserViewSet(viewsets.ModelViewSet):
         if not check_password(password,user.password): 
             return Response({"error":"wrong information"},status=status.HTTP_401_UNAUTHORIZED)
         
-        token = RefreshToken.for_user(user) 
+        token = RefreshToken.for_user(user)  # 특정 함수를 실행하고 다음 실행은 로그인된 상태로 하게 하려면 token을 줘야함
         serializer = self.get_serializer(user) 
         return Response(
             status=status.HTTP_200_OK,
@@ -68,13 +67,14 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data,status=status.HTTP_200_OK)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    # 회원가입 ( 회원가입 후에 바로 설문조사 해야 함 bodypart 데이터를 넣기 위해 )
+    # 회원가입 ( 회원가입 후에 바로 설문조사 해야 함 bodypart 데이터를 넣기 위해, 테스트 할 때 이메일 인증부터 받고 오기 )
     @action(methods=['POST'],detail=False,url_path='join',url_name='user-join')
     def join(self,request):
         username = request.data.get('username')
         password = request.data.get('password')
         email = request.data.get('email')
         fullname = request.data.get('fullname')
+
         if self.id_valid_input(username):
             return Response({"error":"아이디는 2~10자의 한글이나 영문만 사용 가능합니다."},status=status.HTTP_401_UNAUTHORIZED)
         if self.pwd_valid_input(password):
@@ -89,15 +89,32 @@ class UserViewSet(viewsets.ModelViewSet):
             if check_password(password,check_user.password):
                 return Response({"error":"이미 존재하는 비밀번호입니다."},status=status.HTTP_401_UNAUTHORIZED)
         
+        try: # 이전에 이메일 인증을 하고 와야함
+            email_model = Email.objects.get(email=email)
+        except Email.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if not email_model.is_verified: # 이메일 인증안했으면 막힘
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+
+        # 수정하기
         user = User.objects.create_user(username =username,
                                         password=password,
                                         email = email,
                                         fullname = fullname,
                                         )
         user.save()
-        
+        token = RefreshToken.for_user(user)
         serializer = self.get_serializer(user)
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+        email_model.delete()
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data={
+                "token":str(token.access_token), 
+                "user":serializer.data,
+            }
+        )
     
     # 설문조사 - user 아픈 부위 넣기 ( 회원가입 후에 바로 이루어짐, 로그인 한 상태로 이루어져야 함 ), 회원가입 후에 바로 로그인이 되도록 어케 함?
     @action(methods=['PUT'],detail=False,url_path='survey',url_name='user-survey') 
@@ -142,15 +159,21 @@ class UserViewSet(viewsets.ModelViewSet):
         elif User.objects.filter(email=email,fullname=fullname).count()==0:
             return Response({"error":"이메일과 일치하는 실명이 아닙니다"},status=status.HTTP_401_UNAUTHORIZED)
         
-        try:
-            user = User.objects.get(email=email,fullname=fullname)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_407_PROXY_AUTHENTICATION_REQUIRED)
+        try: # 이전에 이메일 인증을 하고 와야함
+            email_model = Email.objects.get(email=email)
+        except Email.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+        if not email_model.is_verified:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         
+        email_model.delete()
+        user = User.objects.get(email=email,fullname=fullname)
         serializer = UserSerializer(user)
+        
         return Response(serializer.data,status=status.HTTP_200_OK)
     
-    # 비밀번호 찾기 / 재설정
+    # 비밀번호 찾기
     @action(methods=['GET'],detail = False, url_path='find_pwd',url_name='user-findpwd')
     def find_pwd(self,request):
         username = request.data.get('username')
@@ -163,12 +186,33 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response({"error":"아이디와 일치하는 이메일이 아닙니다!"},status=status.HTTP_401_UNAUTHORIZED)
         elif User.objects.filter(email=email,username=username, fullname = fullname).count()==0:
             return Response({"error":"이메일과 아이디와 일치하는 실명이 아닙니다 !"},status=status.HTTP_401_UNAUTHORIZED)
-        try:
-            user = User.objects.get(email=email,username = username,fullname=fullname)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        # 비밀번호 재설정
+        try: # 이전에 이메일 인증을 하고 와야함
+            email_model = Email.objects.get(email=email)
+        except Email.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if not email_model.is_verified:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        email_model.delete()
+        user = User.objects.get(username=username,email=email)
+        serializer = UserSerializer(user)
+        token = RefreshToken.for_user(user)
+        return Response(
+            status=status.HTTP_202_ACCEPTED,
+            data={
+                "token":str(token.access_token), 
+                "user":serializer.data,
+            }
+        )
+    
+    # 비밀번호 재설정 ( 비밀번호 찾기 화면에서 이메일 인증을 했으면 로그인이 되도록 )
+    @action(methods=['PUT'],detail = False, url_path='reset_pwd',url_name='user-findpwd')
+    def reset_pwd(self,request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
         password = request.data.get('password')
         if self.pwd_valid_input(password):
             return Response({"error":"비밀번호는 8~15자의 영문, 숫자, 특수문자 2가지 이상 조합으로 사용 가능합니다."},status=status.HTTP_401_UNAUTHORIZED)
@@ -180,19 +224,27 @@ class UserViewSet(viewsets.ModelViewSet):
         if password != re_password:
             return Response({"error":"비밀번호가 다릅니다."},status=status.HTTP_406_NOT_ACCEPTABLE)
 
-        user.set_password(password)
-        user.save()
-        serializer = self.get_serializer(user)
+        request.user.set_password(password)
+        request.user.save()
+        serializer = self.get_serializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
         
     # 이메일 보내는 함수
     @action(methods=['GET'],detail=False,url_path='send',url_name='email-send')
     def email_send(self,request):
-        subject = "자세차렷 이메일 입니다."
-        message = f'이메일 인증 코드는 < {self.random_num} > 입니다.'
+        random_num = f"{randint(0,999999):06}" 
+        subject = "자세차렷 인증 이메일 입니다."
+        message = f'이메일 인증 코드는 < {random_num} > 입니다.'
         from_email = "jh010303@naver.com"
         recipient_list = [request.data.get('email')]
+
+        if Email.objects.filter(email = recipient_list).count() >0:
+            email = Email.objects.get(email = recipient_list)
+            email.delete()
+
+        email = Email.objects.create(verify_num = random_num,email=recipient_list[0])
+        email.save()
 
         try:
             send_mail(subject=subject,message=message,from_email=from_email,recipient_list=recipient_list,fail_silently=False)
@@ -200,12 +252,20 @@ class UserViewSet(viewsets.ModelViewSet):
         except Exception as e: # 서버 오류 시 예외 처리
             return Response({"error": "Email send error"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
     
-    # 이메일 인증번호 확인
+    # 이메일 인증번호 확인 -> 이메일 모델을 만들어서 해결
     @action(methods=['GET'],detail=False,url_path='verify',url_name='email-verify')
     def email_verify(self,request):
-        print("verify",request.data.get('verify'))
-        print(self.random_num)
-        if request.data.get('verify') == self.random_num:
+        email = request.data.get('email')
+        verify = request.data.get('verify')
+
+        try: # 이전에 이메일 인증을 하고 와야함
+            email_model = Email.objects.get(email=email)
+        except Email.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if verify == email_model.verify_num:
+            email_model.is_verified = True
+            email_model.save()
             return Response({"성공":"인증번호를 확인하였습니다."},status=status.HTTP_202_ACCEPTED)
         else:
             return Response({"오류":"인증번호가 올바르지 않습니다."},status=status.HTTP_406_NOT_ACCEPTABLE)
@@ -235,6 +295,7 @@ class VideoLikeListAPIView(ListAPIView):
             return Response(videolikeserializer.data,status=status.HTTP_200_OK)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
+# 동영상 좋아요를 접근하기 위해 동영상 id 사용
 class VideoLikeRetrieveAPIView(RetrieveUpdateDestroyAPIView,CreateAPIView): 
     queryset = VideoLike.objects.all()
     serializer_class = VideoLikeSerializer
