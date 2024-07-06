aa4d53cd0bfdf78a4615274d05ce63929a7f24ac
JUNE <jh010303@naver.com>
2024-07-06 18:39:07 +0900

    feat : videos/urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 urlpatterns = [
-    path('bodypart/',views.BodyPartListAPIView.as_view(),name = 'bodypart-list'),
-    path('<int:bodypart_id>/bodypart_id/',views.BodyPartRetrieveAPIView.as_view(),name = 'bodypart-retrieve'),
-    path('',views.VideoListAPIView.as_view(),name = 'video-list'),
-    path('<int:video_id>/video_id/',views.VideoRetrieveAPIView.as_view(),name = 'video-retrieve'),
 ]
+
+router = DefaultRouter()
+router.register(r'videos',views.VideoViewSet)
+urlpatterns+=router.urls
