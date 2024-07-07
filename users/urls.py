9828b275fb38fceecc46fd4af4234a455cbc596d
leeeemj <leemj2944@gmail.com>
2024-07-07 23:33:08 +0900

    feat : 소셜 로그인

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,20 @@ from users import views
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
+
 urlpatterns = [
     path('bodypart/',views.BodyPartListAPIView.as_view(),name = 'bodypart-list'),
     path('videolike/',views.VideoLikeListAPIView.as_view(),name = 'videolike-list'),
     path('video_id/<int:video_id>/',views.VideoLikeRetrieveAPIView.as_view(),name = 'videolike-retrieve'),
+    #kakao
+    path('kakao/login/', views.kakao_login, name='kakao_login'),
+    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
+    path('kakao/login/finish/', views.KaKaoLogin.as_view(),name='kakao_login_todjango'),
+    #naver
+    # path('naver/login', views.naver_login, name='naver_login'),
+    # path('naver/callback/', views.naver_callback, name='naver_callback'),
+    # path('naver/login/finish/', views.NaverLogin.as_view(), name='naver_login_todjango'),
+
 ]
 
 router = DefaultRouter()
