5292723288c632d154ce19ece6fcd775d6200f88
JUNE <jh010303@naver.com>
2024-07-08 17:24:41 +0900

    feat : users/views.py
    response 변경

--- users/views.py
+++ users/views.py
@@ -92,10 +92,10 @@ class UserViewSet(viewsets.ModelViewSet):
         try: # 이전에 이메일 인증을 하고 와야함
             email_model = Email.objects.get(email=email)
         except Email.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response({"error":"이메일 인증을 받지 않았습니다."},status=status.HTTP_401_UNAUTHORIZED)
         
         if not email_model.is_verified: # 이메일 인증안했으면 막힘
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"error":"이메일 인증을 받지 않았습니다."},status=status.HTTP_401_UNAUTHORIZED)
         
 
         # 수정하기
@@ -107,7 +107,7 @@ class UserViewSet(viewsets.ModelViewSet):
         user.save()
         token = RefreshToken.for_user(user)
         serializer = self.get_serializer(user)
-        # email_model.delete()
+        email_model.delete()
         return Response(
             status=status.HTTP_201_CREATED,
             data={
@@ -236,11 +236,11 @@ class UserViewSet(viewsets.ModelViewSet):
         random_num = f"{randint(0,999999):06}" 
         subject = "자세차렷 인증 이메일 입니다."
         message = f'이메일 인증 코드는 < {random_num} > 입니다.'
-        from_email = "jh010303@naver.com"
+        from_email = "likelion12@naver.com"
         recipient_list = [request.data.get('email')]
 
-        if Email.objects.filter(email = recipient_list).count() >0:
-            email = Email.objects.get(email = recipient_list)
+        if Email.objects.filter(email = recipient_list[0]).count() >0:
+            email = Email.objects.get(email = recipient_list[0])
             email.delete()
 
         email = Email.objects.create(verify_num = random_num,email=recipient_list[0])
@@ -336,93 +336,3 @@ class VideoLikeRetrieveAPIView(RetrieveUpdateDestroyAPIView,CreateAPIView):
         videolike.save()
         serializer = self.get_serializer(videolike)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-# ==========================================================================================
-#                                       KaKao 
-# ========================================================================================== 
-from django.conf import settings
-from django.shortcuts import redirect
-from users.models import User
-from allauth.socialaccount.models import SocialAccount
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.kakao import views as kakao_view
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from django.http import JsonResponse
-import requests
-from rest_framework import status
-from json.decoder import JSONDecodeError
-
-BASE_URL = 'http://127.0.0.1:8000/'
-KAKAO_CALLBACK_URI = BASE_URL + 'kakao/callback/'
-state=getattr(settings,'STATE')
-def kakao_login(request):
-    rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
-    return redirect(
-        f"https://kauth.kakao.com/oauth/authorize?client_id={rest_api_key}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code"
-    )
-
-def kakao_callback(request):
-    rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
-    code = request.GET.get("code")
-    redirect_uri = KAKAO_CALLBACK_URI
-    """
-    Access Token Request
-    """
-    token_req = requests.get(
-        f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={rest_api_key}&redirect_uri={redirect_uri}&code={code}")
-    token_req_json = token_req.json()
-    error = token_req_json.get("error")
-    if error is not None:
-        raise JSONDecodeError(error)
-    access_token = token_req_json.get("access_token")
-    """
-    Email Request
-    """
-    profile_request = requests.get(
-        "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"})
-    profile_json = profile_request.json()
-    kakao_account = profile_json.get('kakao_account')
-    
-
-    email = kakao_account.get('email')
-    # username=kakao_account.get('name')
-    """
-    Signup or Signin Request
-    """
-    try:
-        user = User.objects.get(email=email)
-        # 기존에 가입된 유저의 Provider가 kakao가 아니면 에러 발생, 맞으면 로그인
-        # 다른 SNS로 가입된 유저
-        social_user = SocialAccount.objects.get(user=user)
-        if social_user is None:
-            return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
-        if social_user.provider != 'kakao':
-            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
-        # 기존에 Google로 가입된 유저
-        # data = {'access_token': access_token, 'code': code}
-        # accept = requests.post(
-        #     f"{BASE_URL}accounts/kakao/login/finish/", data=data)
-        # accept_status = accept.status_code
-        # if accept_status != 200:
-        #     return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
-        # accept_json = accept.json()
-        # accept_json.pop('user', None)
-        # return JsonResponse(accept_json)
-    except User.DoesNotExist:
-        # 기존에 가입된 유저가 없으면 새로 가입
-        data = {'access_token': access_token, 'code': code}
-        accept = requests.post(
-            f"{BASE_URL}kakao/login/finish/", data=data)
-        accept_status = accept.status_code
-        if accept_status != 200:
-            return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
-        # user의 pk, email, first name, last name과 Access Token, Refresh token 가져옴
-        accept_json = accept.json()
-        # accept_json.pop('user', None)
-        return JsonResponse(accept_json)
-    
-    
-class KaKaoLogin(SocialLoginView):
-    adapter_class = kakao_view.KakaoOAuth2Adapter
-    client_class = OAuth2Client
-    callback_url = KAKAO_CALLBACK_URI
