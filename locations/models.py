3efc82f56f2d0769a82140e3f06622b860c9f571
JUNE <jh010303@naver.com>
2024-07-08 19:24:08 +0900

    fix : locations/models.py
    병원 모델 - name, address에 null=true 추가 / Lesson 모델 - name, address에 null=true 추가

--- locations/models.py
+++ locations/models.py
@@ -3,20 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Hospital(models.Model):
-    name=models.CharField(max_length=50)
-    address=models.CharField(max_length=50)
+    name=models.CharField(max_length=50,null=True)
+    address=models.CharField(max_length=50,null=True)
     number=models.CharField(max_length=50,null=True)
     time=models.CharField(max_length=50,null=True)
-    image=models.ImageField(upload_to='hospitals/',max_length=None,null=True) #이미지 없을 시 넣을 디폴트 이미지
     longitude=models.DecimalField(max_digits=10, decimal_places=6)
     latitude=models.DecimalField(max_digits=10, decimal_places=6)
 
 class Lesson(models.Model):
-    name=models.CharField(max_length=50)
-    address=models.CharField(max_length=50)
+    name=models.CharField(max_length=50,null=True)
+    address=models.CharField(max_length=50,null=True)
     number=models.CharField(max_length=50,null=True)
     time=models.CharField(max_length=50,null=True)
-    image=models.ImageField(upload_to='lessons/',max_length=None,null=True)
     longitude=models.DecimalField(max_digits=10, decimal_places=6)
     latitude=models.DecimalField(max_digits=10, decimal_places=6)
 
