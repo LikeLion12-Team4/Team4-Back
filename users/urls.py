3ca98683c1b5b73ec8b40c4a930be82a9d787e30
JUNE <jh010303@naver.com>
2024-07-09 21:08:12 +0900

    feat : users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('bodypart/',views.BodyPartListAPIView.as_view(),name = 'bodypart-list'),
     path('videolike/',views.VideoLikeListAPIView.as_view(),name = 'videolike-list'),
     path('video_id/<int:video_id>/',views.VideoLikeRetrieveAPIView.as_view(),name = 'videolike-retrieve'),
-    path('email/',views.EmailRetrieveAPIView.as_view(),name = "email-retrieve"),
+    path('email/send/',views.send_email,name = "email-retrieve"),
+    path('email/verify/',views.verify_email,name = "email-retrieve"),
 ]
-
 router = DefaultRouter()
 router.register(r'users',views.UserViewSet)
 urlpatterns+=router.urls
