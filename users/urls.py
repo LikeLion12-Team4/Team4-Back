4dbd7ffb1f980f0b66777ee81a5ff642fc4f7416
JUNE <jh010303@naver.com>
2024-07-10 23:56:15 +0900

    feat : users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     path('bodypart/<int:bodypart_id>/',views.delete_bodypart,name = 'bodypart-delete'),
     path('videolike/',views.VideoLikeListAPIView.as_view(),name = 'videolike-list'),
     path('video_id/<int:video_id>/',views.VideoLikeRetrieveAPIView.as_view(),name = 'videolike-retrieve'),
-    path('email/send/',views.send_email,name = "email-retrieve"),
-    path('email/verify/',views.verify_email,name = "email-retrieve"),
+    path('email/send/',views.send_email,name = "email-send"),
+    path('email/verify/',views.verify_email,name = "email-verify"),
     #kakao
     path("kakao/login/",views.kakao_login,name = "kakao-callback"),
     path("kakao/login/finish/",views.KakaoLoginView.as_view()),
     #naver
-    path("naver/login/",views.naver_login,name = "kakao-callback"),
+    path("naver/login/",views.naver_login,name = "naver-callback"),
     path("naver/login/finish/",views.NaverLoginView.as_view()),
 
 ]
