79643e6b00c4e68bf1841fc97b34579e99da6883
JUNE <jh010303@naver.com>
2024-07-10 20:58:26 +0900

    Fix : videos/views.py
    webcam 삭제

--- videos/urls.py
+++ videos/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 urlpatterns = [
-    path('webcam/',views.open_webcam,name = 'webcam'),
 ]
 
 router = DefaultRouter()
