c8bdf2831c2572471ef588eb8b9b9ccdd190527a
202010850 김준형 <jh010303@naver.com>
2024-07-12 10:33:30 +0900

    feat : config/settings.py
    서버랑 데이터베이스 연결

--- config/settings.py
+++ config/settings.py
@@ -201,15 +201,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# from config import local_settings
-# DATABASES = local_settings.DATABASES
+from config import local_settings
+DATABASES = local_settings.DATABASES
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
